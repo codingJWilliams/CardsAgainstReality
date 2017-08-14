03858a0d384f34fa2aa9e44cb0993082cf6c74d5
Jay Williams <course@jaywilliams.me>
2017-08-14 20:49:00 +0100

    I give up! (For today)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var jsonfile = require('jsonfile')
 var file = 'storage/games.json'
+var rs = require("randomstring");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/lobby.html');
@@ -10,13 +11,20 @@ app.get('/', function(req, res){
 app.get("/party-settings/", function(req, res){
   res.sendFile(__dirname + "/html/party-settings.html")
 })
+function generateGameCode(){
+  return "CAH-" + rs.generate({
+    "length": 4,
+    "charset": "ABCDEFGHJKPQRSTWXYZabcdefghjkpqrstwxyz123456789-_"
+  })
+}
 app.get("/new-game/", function (req, res){
   var games = jsonfile.readFileSync(file);
-  var nsfw = req.query.nsfw
+  var nsfw = req.query.nsfw;
+  games.concat([{code: generateGameCode(), nsfw: nsfw}])
 })
 io.on('connection', function(socket){
   console.log('User joined your channel');
-  socket.on("create-new-gam")
+  socket.on("join-game")
 });
 
 http.listen(3000, function(){
